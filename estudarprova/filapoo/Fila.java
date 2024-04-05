a7f859acb6342e5e8b9fb5fd301c15447b80a6f8
Juan Santos <107580945+JuanSantos64@users.noreply.github.com>
2024-04-05 13:23:30 -0300

    Add fila e pesquisa binaria

--- estudarprova/filapoo/Fila.java
+++ estudarprova/filapoo/Fila.java
@@ -10,4 +10,45 @@ package estudarprova.filapoo;
  */
 public class Fila {
     
+    private int[] valores;
+    private int primeiro;
+    private int ultimo;
+    private int total;
+
+    public Fila() {
+        valores = new int[10];
+        primeiro = 0;
+        ultimo = 0;
+        total = 0;
+    }
+
+    public void inserir(int elemento) {
+        if (isFull()) {
+            throw new RuntimeException("Fila cheia! Não da pra inserir mais ninguém");
+        }
+        valores[ultimo] = elemento;
+        ultimo = (ultimo + 1) % valores.length;
+        total++;
+    }
+
+    public int retirar() {
+
+        if (isEmpty()) {
+            throw new RuntimeException("Fila está Vazia! Não tem ninguém para retirar");
+        }
+
+        int elemento = valores[primeiro];
+        primeiro = (primeiro + 1) % valores.length;
+        total--;
+        return elemento;
+
+    }
+
+    public boolean isEmpty() {
+        return total == 0;
+    }
+
+    public boolean isFull() {
+        return total == valores.length;
+    }
 }
